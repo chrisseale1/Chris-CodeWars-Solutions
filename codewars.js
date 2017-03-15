189a187fa2c10fd456df3e01cf1ef6f4da2e99b4
Chris Seale <sealechris@gmail.com>
2017-03-15 16:52:30 -0500

    more problems, less money

--- codewars.js
+++ codewars.js
@@ -1,4 +1,4 @@
-//1.   Convert boolean values to strings 'Yes' or 'No'.
+//#1   Convert boolean values to strings 'Yes' or 'No'.
 /*Complete the bool_to_word (Javascript: boolToWord ) method.
 
 Given: a boolean value
@@ -17,3 +17,48 @@ function boolToWord( bool ){
 function boolToWord(bool){
     return bool ? "Yes" : "No";
 }
+
+
+
+//#2     EVEN OR ODD
+/* Instructions
+Output
+Past Solutions
+Create a function that takes an integer as an argument and
+ returns "Even" for even numbers or "Odd" for odd numbers. */
+
+function even_or_odd(number) {
+  if(number % 2 !== 0){
+    return "Odd";
+  } else {
+    return "Even";
+  }
+}
+//or
+function even_or_odd(number){
+    return (number % 2 !== 0) ? "Odd" : "Even";
+}
+
+
+
+//#3    Remove exclamation marks
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+  return s.replace(/["!"]/gi, "");
+}
+
+
+//#4    Push a hash/an object into array
+/*You are trying to put an object into an array, but it always returns error, 
+solve it and keep it as simple as possible! */
+/* Given problem: 
+items = [] 
+items.push {a: "b", c: "d"}  */
+
+let items = [];
+items.push ({a: "b", c: "d"});
+
+
+
+//#5   
